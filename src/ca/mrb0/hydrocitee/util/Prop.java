98e2a0189e3504014d5136b59b325266f1bbb917
Mike Burke <mrburke@gmail.com>
2012-03-11 22:04:56 -0300

    Make SongValues immutable

--- src/ca/mrb0/hydrocitee/util/Prop.java
+++ src/ca/mrb0/hydrocitee/util/Prop.java
@@ -59,4 +59,36 @@ public class Prop<T> {
 	public static interface ValidityTest<V> {
 		public boolean isValid(V newValue);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Prop<?> other = (Prop<?>) obj;
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		} else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
 }
