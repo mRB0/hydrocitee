adb5bf365be04701c975be97530aa999526a6c2d
Mike Burke <mrburke@gmail.com>
2012-03-12 21:25:33 -0300

    Make objects completely immutable. I was thinking and decided to remove the whole Prop/freeze paradigm, because it's kind of awkward, quite unpredictable, and doesn't really save any time.  The rationale behind it was to let me adjust values after construction, and then freeze them before publishing the object, while also being able to test the values that get set.  Having constructors that take all the fields works just as well, even though some of these constructors are quite long (SongValues, ITInstrument).  Anyway, this should eventually be handled by helpers that do the construction for users, so the end gain is minimal, and it's more desirable to avoid making the user call .get() for every property.

--- src/ca/mrb0/hydrocitee/it/ITEnvelope.java
+++ src/ca/mrb0/hydrocitee/it/ITEnvelope.java
@@ -1,54 +1,60 @@
 package ca.mrb0.hydrocitee.it;
 
-import ca.mrb0.hydrocitee.util.Prop;
 import fj.data.List;
 
 public class ITEnvelope {
-	public final Prop<Boolean> enabled = new Prop<Boolean>();
-	public final Prop<Boolean> loop = new Prop<Boolean>();
-	public final Prop<Boolean> susloop = new Prop<Boolean>();
+	public final boolean enabled;
+	public final boolean loop;
+	public final boolean susloop;
 	
-	public final Prop<Integer> loopBegin = new Prop<Integer>();
-	public final Prop<Integer> loopEnd = new Prop<Integer>();
-	public final Prop<Integer> susloopBegin = new Prop<Integer>();
-	public final Prop<Integer> susloopEnd = new Prop<Integer>();
+	public final int loopBegin;
+	public final int loopEnd;
+	public final int susloopBegin;
+	public final int susloopEnd;
 	
-	public final Prop<List<NodePoint>> nodes = new Prop<List<NodePoint>>(List.<NodePoint>list());
-	
-	protected ITEnvelope() {
+	public final List<NodePoint> nodes;
+
+	protected ITEnvelope(boolean enabled, boolean loop, boolean susloop,
+            int loopBegin, int loopEnd, int susloopBegin, int susloopEnd,
+            List<NodePoint> nodes) {
+        super();
+        this.enabled = enabled;
+        this.loop = loop;
+        this.susloop = susloop;
+        this.loopBegin = loopBegin;
+        this.loopEnd = loopEnd;
+        this.susloopBegin = susloopBegin;
+        this.susloopEnd = susloopEnd;
+        this.nodes = nodes;
+    }
+
+    protected static ITEnvelope newFromData(byte data[], int offs) {
+        boolean enabled;
+        boolean loop;
+        boolean susloop;
+        
+        int loopBegin;
+        int loopEnd;
+        int susloopBegin;
+        int susloopEnd;
+        
+        List<NodePoint> nodes;
 		
-	}
-
-	protected ITEnvelope(ITEnvelope copy) {
-		enabled.set(copy.enabled.get());
-		loop.set(copy.loop.get());
-		susloop.set(copy.susloop.get());
-
-		loopBegin.set(copy.loopBegin.get());
-		loopEnd.set(copy.loopEnd.get());
-		susloopBegin.set(copy.susloopBegin.get());
-		susloopEnd.set(copy.susloopEnd.get());
-
-		nodes.set(copy.nodes.get());
-	}
-	
-	protected static ITEnvelope newFromData(byte data[], int offs) {
-		ITEnvelope env = new ITEnvelope();
-		
+        
 		int flags = 0xff & data[offs++];
-		env.enabled.set((flags & 0x1) != 0);
-		env.loop.set((flags & 0x2) != 0);
-		env.susloop.set((flags & 0x3) != 0);
+		enabled = (flags & 0x1) != 0;
+		loop = (flags & 0x2) != 0;
+		susloop = (flags & 0x3) != 0;
 		
 		int numPoints = 0xff & data[offs++];
-		env.nodes.set(List.<NodePoint>replicate(numPoints, null));
+		nodes = List.<NodePoint>replicate(numPoints, null);
 		
-		env.loopBegin.set(0xff & data[offs++]);
-		env.loopEnd.set(0xff & data[offs++]);
-		env.susloopBegin.set(0xff & data[offs++]);
-		env.susloopEnd.set(0xff & data[offs++]);
+		loopBegin = 0xff & data[offs++];
+		loopEnd = 0xff & data[offs++];
+		susloopBegin = 0xff & data[offs++];
+		susloopEnd = 0xff & data[offs++];
 		
-		return env;
+		return new ITEnvelope(enabled, loop, susloop, loopBegin, loopEnd, susloopBegin, susloopEnd, nodes);
 	}
 
 	public static class NodePoint {
@@ -61,107 +67,64 @@ public class ITEnvelope {
 			this.tick = tick;
 		}
 	}
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (enabled ? 1231 : 1237);
+        result = prime * result + (loop ? 1231 : 1237);
+        result = prime * result + loopBegin;
+        result = prime * result + loopEnd;
+        result = prime * result + ((nodes == null) ? 0 : nodes.hashCode());
+        result = prime * result + (susloop ? 1231 : 1237);
+        result = prime * result + susloopBegin;
+        result = prime * result + susloopEnd;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ITEnvelope other = (ITEnvelope) obj;
+        if (enabled != other.enabled) {
+            return false;
+        }
+        if (loop != other.loop) {
+            return false;
+        }
+        if (loopBegin != other.loopBegin) {
+            return false;
+        }
+        if (loopEnd != other.loopEnd) {
+            return false;
+        }
+        if (nodes == null) {
+            if (other.nodes != null) {
+                return false;
+            }
+        } else if (!nodes.equals(other.nodes)) {
+            return false;
+        }
+        if (susloop != other.susloop) {
+            return false;
+        }
+        if (susloopBegin != other.susloopBegin) {
+            return false;
+        }
+        if (susloopEnd != other.susloopEnd) {
+            return false;
+        }
+        return true;
+    }
 	
-	protected void freeze() {
-		enabled.freeze();
-		loop.freeze();
-		susloop.freeze();
-
-		loopBegin.freeze();
-		loopEnd.freeze();
-		susloopBegin.freeze();
-		susloopEnd.freeze();
-
-		nodes.freeze();
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((enabled == null) ? 0 : enabled.hashCode());
-		result = prime * result + ((loop == null) ? 0 : loop.hashCode());
-		result = prime * result
-				+ ((loopBegin == null) ? 0 : loopBegin.hashCode());
-		result = prime * result + ((loopEnd == null) ? 0 : loopEnd.hashCode());
-		result = prime * result + ((nodes == null) ? 0 : nodes.hashCode());
-		result = prime * result + ((susloop == null) ? 0 : susloop.hashCode());
-		result = prime * result
-				+ ((susloopBegin == null) ? 0 : susloopBegin.hashCode());
-		result = prime * result
-				+ ((susloopEnd == null) ? 0 : susloopEnd.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		ITEnvelope other = (ITEnvelope) obj;
-		if (enabled == null) {
-			if (other.enabled != null) {
-				return false;
-			}
-		} else if (!enabled.equals(other.enabled)) {
-			return false;
-		}
-		if (loop == null) {
-			if (other.loop != null) {
-				return false;
-			}
-		} else if (!loop.equals(other.loop)) {
-			return false;
-		}
-		if (loopBegin == null) {
-			if (other.loopBegin != null) {
-				return false;
-			}
-		} else if (!loopBegin.equals(other.loopBegin)) {
-			return false;
-		}
-		if (loopEnd == null) {
-			if (other.loopEnd != null) {
-				return false;
-			}
-		} else if (!loopEnd.equals(other.loopEnd)) {
-			return false;
-		}
-		if (nodes == null) {
-			if (other.nodes != null) {
-				return false;
-			}
-		} else if (!nodes.equals(other.nodes)) {
-			return false;
-		}
-		if (susloop == null) {
-			if (other.susloop != null) {
-				return false;
-			}
-		} else if (!susloop.equals(other.susloop)) {
-			return false;
-		}
-		if (susloopBegin == null) {
-			if (other.susloopBegin != null) {
-				return false;
-			}
-		} else if (!susloopBegin.equals(other.susloopBegin)) {
-			return false;
-		}
-		if (susloopEnd == null) {
-			if (other.susloopEnd != null) {
-				return false;
-			}
-		} else if (!susloopEnd.equals(other.susloopEnd)) {
-			return false;
-		}
-		return true;
-	}
 	
 }
--- src/ca/mrb0/hydrocitee/it/ITVolumeEnvelope.java
+++ src/ca/mrb0/hydrocitee/it/ITVolumeEnvelope.java
@@ -5,35 +5,35 @@ import fj.data.List;
 
 
 public class ITVolumeEnvelope extends ITEnvelope {
+    
+    private static List<ITEnvelope.NodePoint> emptyNodeList() {
+        return List.<ITEnvelope.NodePoint>list(new NodePoint(64, 0), new NodePoint(64, 100));
+    }
+    
 	public ITVolumeEnvelope() {
-		super();
-		nodes.set(List.<ITEnvelope.NodePoint>list(new NodePoint(64, 0), new NodePoint(64, 100)));
+	    this(false, false, false, 0, 1, 0, 0, emptyNodeList());
 	}
+	
+    public ITVolumeEnvelope(boolean enabled, boolean loop, boolean susloop,
+            int loopBegin, int loopEnd, int susloopBegin, int susloopEnd,
+            List<NodePoint> nodes) {
+        super(enabled, loop, susloop, loopBegin, loopEnd, susloopBegin, susloopEnd,
+                nodes);
+    }
 
-	private ITVolumeEnvelope(ITEnvelope copy) {
-		super(copy);
-		freeze();
-	}
-
-	public ITVolumeEnvelope(ITVolumeEnvelope copy) {
-		super(copy);
-		freeze();
-	}
-
-	public static ITVolumeEnvelope newFromData(byte data[], int offs) {
-		ITVolumeEnvelope env = new ITVolumeEnvelope(ITEnvelope.newFromData(
-				data, offs));
-
-		int numPoints = env.nodes.get().length();
-		env.nodes.set(List.<NodePoint>list());
+    public static ITVolumeEnvelope newFromData(byte data[], int offs) {
+		ITEnvelope env = ITEnvelope.newFromData(data, offs);
+		
+		int numPoints = env.nodes.length();
+		
+		List<NodePoint> nodes = List.<NodePoint>list();
 		for (int i = 0; i < numPoints; i++) {
 			int val = 0xff & data[offs + 6 + i*3];
 			int tick = Streams.unpack16(data, offs + 6 + i*3 + 1);
 			
-			env.nodes.set(env.nodes.get().snoc(new NodePoint(val, tick)));
+			nodes = nodes.snoc(new NodePoint(val, tick));
 		}
 
-		env.freeze();
-		return env;
+		return new ITVolumeEnvelope(env.enabled, env.loop, env.susloop, env.loopBegin, env.loopEnd, env.susloopBegin, env.susloopEnd, nodes);
 	}
 }
